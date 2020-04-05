c66b5207652c6e5576d83ea17611ce27ce51355b
Kalyani-krt <kr.tarke@gmail.com>
2020-04-05 10:39:57 +0530

    added snap screen

--- app/src/main/java/com/ananddevelopment/fitness_care/MainActivity.java
+++ app/src/main/java/com/ananddevelopment/fitness_care/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ananddevelopment.fitness_care;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.IntRange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +14,20 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private static  final int splash=2000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Home_page.class);
+                startActivity(intent);
+            }
+        },splash);
 
 
       
