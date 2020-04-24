762c42c91c59b6637bf78a9ef7a919d4955dbbc7
Kalyani-krt <kr.tarke@gmail.com>
2020-04-24 17:41:18 +0530

    exercise_1stLayout

--- app/src/main/java/com/ananddevelopment/fitness_care/exercise.java
+++ app/src/main/java/com/ananddevelopment/fitness_care/exercise.java
@@ -5,16 +5,12 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class exercise extends AppCompatActivity {
-    ImageView ivexercise1,ivexercise2,ivexercise3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
-        ivexercise1=findViewById(R.id.ivexercise1);
-        ivexercise2=findViewById(R.id.ivexercise2);
-        ivexercise3=findViewById(R.id.ivexercise3);
 
     }
 }
