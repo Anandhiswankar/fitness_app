a1aac4f64853162ec7849301c1f828617a01fd7e
Anandhiswankar <anandhiswankar3@gmail.com>
2020-04-26 16:13:11 +0530

    New Ui and Layout with Connection

--- app/src/main/java/com/ananddevelopment/fitness_care/diet.java
+++ app/src/main/java/com/ananddevelopment/fitness_care/diet.java
@@ -8,17 +8,28 @@ import android.widget.Button;
 
 public class diet extends AppCompatActivity {
 
-    Button btnGain;
+    Button btnGain,btnlose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet);
+        btnlose=findViewById(R.id.btnlose);
+
         btnGain=findViewById(R.id.btnGain);
-        btnGain.setOnClickListener(new View.OnClickListener() {
+
+        btnlose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(diet.this,diet_Plans.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(diet.this,diet_Plans_gain.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/ananddevelopment/fitness_care/diet_Plans_gain.java
@@ -0,0 +1,13 @@
+package com.ananddevelopment.fitness_care;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class diet_Plans_gain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_plain_gain);
+    }
+}
